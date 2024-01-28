a0fbaefeb1eb17b5b2d3f0eaff2be597acf1e408
Hakim Ghods <ghods.hakim@hotmail.com>
2024-01-28 18:49:40 +0330

    Hub

---
+++ src/WebApp/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApp.Hubs;
+
+public class ChatHub:Hub
+{
+    public async Task BroadcastMessage(string message)
+    {
+        await Clients.Others.SendAsync(message);
+    }
+}
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,6 +1,10 @@
+using WebApp.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapHub<ChatHub>("chathub");
 
 app.Run();
